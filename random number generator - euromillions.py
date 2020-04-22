2eb5f2bafbffad3aa6242ac1d933d2d22b0c339e
ric9rdo <37312376+ric9rdo@users.noreply.github.com>
2020-04-22 10:41:43 -0500

    Add files via upload

---
+++ random number generator - euromillions.py
@@ -0,0 +1,20 @@
+# The module random contains the function randint which can be used to generate a random integer. 
+
+# Euromilhoes - numeros
+print("numeros")
+
+from random import randint
+
+for i in range(5):
+    value = randint(1, 50)
+    print(value)
+    
+print("estrelas")
+    
+    # Euromilhoes - estrelas
+    
+from random import randint
+
+for i in range(2):
+    value = randint(1, 9)
+    print(value)
